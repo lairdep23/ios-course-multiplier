9bace559e12a43d354404db749de5e66bd83bbb1
Evan Laird <Evan@Evans-MacBook-Pro.local>
2016-02-04 23:28:07 -0600

    added files for multiplier

--- multiplier/ViewController.swift
+++ multiplier/ViewController.swift
@@ -10,16 +10,85 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //variables
+    var addNumber = 0
+    var addTaps = 0
+    var sumNumber = 0
+    var sumAddNumber = 0
+    var maxNumber = 100
+
+  //Outlets
+
+    @IBOutlet weak var multiplyThisBitch: UIImageView!
+    @IBOutlet weak var multipleTextField: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var pressLabel: UILabel!
+    
+    @IBOutlet weak var addButton: UIButton!
+    
+    @IBOutlet weak var mathLabel: UILabel!
+    
+    @IBAction func pressedPlayButton(sender: UIButton!) {
+        
+        if multipleTextField.text != nil && multipleTextField.text != "" {
+        
+        multiplyThisBitch.hidden = true
+        multipleTextField.hidden = true
+        playButton.hidden = true
+        
+        pressLabel.hidden = false
+        addButton.hidden = false
+            
+        addNumber = Int(multipleTextField.text!)!
+        
+        }
+        }
+    
+    @IBAction func pressedAddButton(sender: UIButton!) {
+        pressLabel.hidden = true
+        
+        mathLabel.hidden = false
+        
+        addTaps++
+        
+        sumNumber = addTaps * addNumber
+        
+        sumAddNumber = sumNumber + addNumber
+        
+        mathLabel.text = "\(sumNumber) + \(addNumber) = \(sumAddNumber)"
+        if gameIsOver(){
+            restartApp()
+        }
     }
+    func restartApp() {
+            multipleTextField.text = ""
+            addNumber = 0
+            addTaps = 0
+            
+            multiplyThisBitch.hidden = false
+            multipleTextField.hidden = false
+            playButton.hidden = false
+            
+            pressLabel.hidden = true
+            addButton.hidden = true
+            
+            mathLabel.hidden = true
+        }
+        
+        func gameIsOver() ->Bool {
+            if sumAddNumber >= maxNumber {
+                return true
+            } else {
+                return false
+            }
+            
+            }
+        }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    
+    
 
-
-}
 
